11f988c2645c1a9b1500f9de153dcbae442a98bb
LalithKamana <kamanav@nexturn.com>
2022-08-21 16:54:13 +0530

    Added 1 more call
    Fixed Issue #1

--- CustomerGRPCClient/Program.cs
+++ CustomerGRPCClient/Program.cs
@@ -5,18 +5,24 @@ using Grpc.Net.Client;
 
 var channel = GrpcChannel.ForAddress("https://localhost:7213");
 var custClient = new Customer.CustomerClient(channel);
-var request = new CustRequestByAuth { Username = "Edwin", Password = "Edwin" };
+var request = new CustRequestByAuth { Username = "Lalith", Password = "Lalith" };
 var response = await custClient.GetSpecificCustomerAsync(request);
 Console.WriteLine($"Welcome {response.Firstname} {response.Lastname} to {response.Company}.");
 Console.WriteLine($"We strongly believe, your {response.Experience} years of experience in {response.Technology} technology will help the company achieve new milestones.");
 Console.WriteLine($"All the very best to you in your {response.Role} role.");
+Console.WriteLine();
+
+var request1 = new CustRequestByAuth { Username = "Edwin", Password = "Edwin" };
+var response1 = await custClient.GetSpecificCustomerAsync(request1);
+Console.WriteLine($"Welcome {response1.Firstname} {response1.Lastname} to {response1.Company}.");
+Console.WriteLine($"We strongly believe, your {response1.Experience} years of experience in {response1.Technology} technology will help the company achieve new milestones.");
+Console.WriteLine($"All the very best to you in your {response1.Role} role.");
 
 using (var call = custClient.GetAllCustomers(new AllCustRequest()))
 {
     await Task.Delay(2000);
     Console.WriteLine();
     Console.WriteLine("fetching customer list......");
-    Console.WriteLine();
     Console.WriteLine();
 
     while (await call.ResponseStream.MoveNext())
